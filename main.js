76b881e280d8290e3583eadc654141f78ac2f1e2
Abhi812163 <76207414+Abhi812163@users.noreply.github.com>
2021-10-25 19:47:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX="";
+noseY="";
+
 function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
@@ -6,16 +9,29 @@ function preload() {
 
 function setup() {
 	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
 	instializeInSetup(mario);
-	canvas.center();
+
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
 }
 
 function draw() {
-	game()
+	game();
 }
 
+function modelLoaded(){
+	console.log("Model is Loaded");
+}
 
-
-
-
-
+function gotPoses(results){
+	if(results.length>0){
+		console.log(results);
+		noseX=results[0].pose.nose.x;
+		noseY=results[0].pose.nose.y;
+	}
+}
