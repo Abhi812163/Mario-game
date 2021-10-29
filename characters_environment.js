3575d25f19f4412f692b640044f8247701661448
Abhi812163 <76207414+Abhi812163@users.noreply.github.com>
2021-10-29 19:34:57 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -221,6 +221,7 @@ function getCoins(coin,character){
   if( character.overlap(coin) && character.live && coin.get==false){
     character.coins+=1;
     coin.get=true;
+    mario_coin.play();
   };
 }
     
@@ -310,6 +311,7 @@ function manualControl(character){
 function jumping(character){
 	if( (noseY<200&&character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
+    mario_jump.play();
 	}
 }
 
@@ -358,6 +360,7 @@ function StepOnEnemy(obj1,obj2){
 	if(obj1_Right>=obj2_Left&&obj1_Left<=obj2_Right && obj1_Down<=obj2_Up+7 && obj1_Down>=obj2_Up-7 && obj2.live==true && obj2.touching.top){
 		obj2.live=false;
     obj1.killing=30;
+    mario_kick.play();
     obj1.kills++;
     if(obj1.velocity.y>=gameConfig.jump*0.8){
       obj1.velocity.y=gameConfig.jump*0.8;
@@ -376,6 +379,9 @@ function die(character){
     character.status="dead";
     character.changeAnimation('dead');
     character.velocity.y-=2;
+    if(character.liveNumber>0){
+      mario_die.play();
+    }
 }
 
 // check character status and response to sprite and game status
@@ -387,6 +393,7 @@ function checkStatus(character){
   }
   if(character.live==false && character.liveNumber==0){
     gameConfig.status="gameover"
+    mario_gameover.play();
   }
 
 }
--- main.js
+++ main.js
@@ -3,6 +3,11 @@ noseY="";
 
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_gameover=loadSound("gameover.wav");
+	mario_kick=loadSound("kick.wav");
+	mario_die=loadSound("mariodie.wav");
 	setSprites();
 	MarioAnimation();
 }
